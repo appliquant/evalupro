c1e1b6784c032a43fa4ea9d16369659edf58f26b
Danny <7348227-rareastro@users.noreply.gitlab.com>
2024-05-05 01:15:01 -0400

    frontend/backend: afficher produits

---
+++ frontend/src/services/productsService.ts
@@ -0,0 +1,34 @@
+import type { ApiResponseType } from '@/types'
+import { handleApiResponse } from '@/services/handleApiResponse'
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+const getProducts = async (jwt: string): Promise<ApiResponseType> => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/api/products`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`
+      }
+    })
+
+    return await handleApiResponse(res)
+  } catch (e) {
+    return {
+      status: 500,
+      message: 'Impossible d\'attendre le serveur lors de la récupérations des produits',
+      errors: [
+        {
+          field: 'network',
+          message:
+            e instanceof Error ? e.message : 'Erreur lors de la communication avec le serveur'
+        }
+      ]
+    } as ApiResponseType
+  }
+}
+
+
+const productsService = { getProducts }
+export { productsService }
