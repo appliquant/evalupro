1e2164940e2900e73aae7d660d5bddc7854504dd
Danny <7348227-rareastro@users.noreply.gitlab.com>
2024-05-15 22:37:16 -0400

    frontend: afficher infos

--- frontend/src/views/managing/ManageEvaluationsTesterView.vue
+++ frontend/src/views/managing/ManageEvaluationsTesterView.vue
@@ -7,15 +7,18 @@
 
         <h2>Liste des mes évaluations</h2>
 
-        <p v-if="data.length <= 0">Aucune évaluation</p>
+        <p v-if="items.length <= 0">Aucune évaluation</p>
 
         <!-- Liste des évaluations -->
         <ul class="list-group" id="list-container">
-          <li class="list-group-item"
-              style="cursor: pointer; user-select: none;"
-              v-for="evaluation in data"
-              :key="evaluation.evaluation.id">
-            {{ evaluation.product.name }}
+          <li
+            style="cursor: pointer; user-select: none;"
+            v-for="(item, index) in items"
+            :key="item.evaluation.id"
+            @click="selectItem(index)"
+            :class="`list-group-item ${selectedItem?.evaluation.id === item.evaluation.id ? 'active' : ''}`"
+          >
+            {{ item.product.name }}
           </li>
         </ul>
       </div>
@@ -24,7 +27,60 @@
         <h2>Évaluation sélectionée</h2>
 
         <form class="needs-validation" novalidate>
+          <fieldset :disabled="selectedItem === null">
+            <!-- Critères d'évaluations -->
+            <div>
+              <div
+                class="mb-3"
+                v-for="criteria in selectedItem?.criterias"
+                :key="criteria.id">
+                <label :for="`criteria-${criteria.id}`" class="form-label d-block">{{ criteria.name }} - Coefficient :
+                  {{ criteria.coefficient }}</label>
+                <select :id="`criteria-${criteria.id}`"
+                        class="form-select"
+                        :value="selectedItem?.criteriasEvaluation.find(c => c.criteriaId === criteria.id)?.value"
+                        v-on:input="e => {
+                          if (selectedItem) {
+                            // selectedItem.criterias.find(c => c.id === criteria.id)!.value = (e.target as HTMLSelectElement).value
+                            // removeErrors('fieldset')
+                          }
+                        }">
+                  <option value="1">Excellent</option>
+                  <option value="0.8">Très bon</option>
+                  <option value="0.6">Acceptable</option>
+                  <option value="0.4">Mauvais</option>
+                  <option value="0.2">Très mauvais</option>
+                </select>
+              </div>
+            </div>
 
+            <!-- Commentaire -->
+            <div>
+              <label for="comment" class="form-label">(Facultatif) Commentaire</label>
+              <textarea id="commentTextArea"
+                        class="form-control"
+                        :value="selectedItem?.evaluation.comment"
+                        v-on:input="e => {
+                         if (selectedItem) {
+                            selectedItem.evaluation.comment = (e.target as HTMLTextAreaElement).value
+                            // removeErrors('comment')
+                         }
+                        }"
+                        aria-describedby="commentInvalidFeedback commentHelpBlock"></textarea>
+
+              <div id="commentInvalidFeedback" class="invalid-feedback"></div>
+              <div id="commentHelpBlock" class="form-text">
+                Le commentaire doit comprendre entre
+                {{ dataLengthValidations.optional_evaluationComment.minlength }}
+                et {{ dataLengthValidations.optional_evaluationComment.maxlength }} caractères.
+              </div>
+            </div>
+
+            <div class="d-flex gap-2">
+              <button type="submit" class="btn btn-primary">Enregistrer</button>
+              <button type="submit" class="btn btn-outline-danger">Supprimer</button>
+            </div>
+          </fieldset>
         </form>
 
       </div>
@@ -33,23 +89,25 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, toRaw } from 'vue'
 import type { ApiResponseType, Category, Criteria, CriteriasEvaluation, Evaluation, Product } from '@/types'
 import { push } from 'notivue'
 import { evaluationsService } from '@/services/evaluationsService'
 import { useAuthStore } from '@/stores/authStore'
+import { dataLengthValidations } from '@/validations'
 
 const authStore = useAuthStore()
 
 type Data = {
   category: Category
   criterias: Criteria[]
-  criteriasEvaluations: CriteriasEvaluation[]
+  criteriasEvaluation: CriteriasEvaluation[]
   evaluation: Evaluation
   product: Product
 }
 
-const data = ref<Data[]>([])
+const items = ref<Data[]>([])
+const selectedItem = ref<Data | null>(null)
 
 
 onMounted(() => {
@@ -67,7 +125,7 @@ onMounted(() => {
         })
       }
 
-      data.value = res.data
+      items.value = res.data
       console.log(res.data)
     } catch (e) {
       const err = e as ApiResponseType
@@ -81,6 +139,22 @@ onMounted(() => {
 
   getMyEvaluationsTester()
 })
+
+const selectItem = (index: number) => {
+  const item = items.value[index]
+
+  if (selectedItem.value?.evaluation.id === item.evaluation.id) {
+    return unSelectItem()
+  }
+
+  selectedItem.value = structuredClone(toRaw(item))
+}
+
+const unSelectItem = () => {
+  selectedItem.value = null
+}
+
+
 </script>
 
 <style lang="scss">
